34a30922422c28277daf8cb1d1fc036cb0486972
Rafael Castro <raphaeldavant@hotmail.com>
2021-05-19 16:44:08 -0300

    feat pg title slide effect

--- js/features.js
+++ js/features.js
@@ -1,5 +1,9 @@
+// FEATURES PG TITLE SLIDE EFFECT WITH ANIMEJS
 anime({
   targets: '.pl-feat-title',
-  // height: 386px;
-  // paddingTop: 30
+  height: 386,
+  paddingTop: 30,
+  delay: 1000,
+  duration: 500,
+  easing: 'cubicBezier(0.0.210, 1.115, 0.360, 1.020)'
 });
