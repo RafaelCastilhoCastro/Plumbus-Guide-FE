2d91ab2f82a4bde60fc5d83e65db7d7dcce6e414
Rafael Castro <raphaeldavant@hotmail.com>
2021-06-02 19:27:34 -0300

    fxd mobile topmenu & overlay

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 // DISPLAY TOGGLE ON OVERLAY AND CONTACT MODAL
-var toggleModal = document.querySelectorAll('.pl-toggle-modal');
+var toggleContactModal = document.querySelectorAll('.pl-toggle-contact-modal');
 
-for (let i = 0; i < toggleModal.length; i++) {
-  toggleModal[i].addEventListener("click", function(){
-    var overlay = document.querySelector('.pl-overlay');
+for (let i = 0; i < toggleContactModal.length; i++) {
+  toggleContactModal[i].addEventListener("click", function(){
+    var contactOverlay = document.querySelector('.pl-contact-overlay');
     var contactModal = document.querySelector('.pl-contact-modal');
-    overlay.classList.toggle('pl-toggleDisplayBlock');
+    contactOverlay.classList.toggle('pl-toggleDisplayBlock');
     contactModal.classList.toggle('pl-Anim-Slide-Top');
   }
 )};
@@ -32,31 +32,34 @@ var btnMenuMobile = document.querySelector('.pl-btn-menu-mobile');
 var btnMenuMobileIcon = document.querySelector('.pl-btn-menu-mobile ion-icon')
 var menuMobile = document.querySelector('.pl-topmenu-mobile');
 var menuMobileOverlay = document.querySelector('.pl-overlay-menu-mobile');
+var toggleMobileMenu = document.querySelectorAll('.pl-toggle-mobile-menu');
 
-btnMenuMobile.addEventListener("click", function(){
-  menuMobile.classList.remove("pl-menu-mob-no-anim");
-  menuMobile.classList.toggle("pl-menu-mob-in");
-  menuMobile.classList.toggle("pl-menu-mob-out");
+for (let i = 0; i < toggleMobileMenu.length; i++) {
+  toggleMobileMenu[i].addEventListener("click", function(){
+    menuMobile.classList.remove("pl-menu-mob-no-anim");
+    menuMobile.classList.toggle("pl-menu-mob-in");
+    menuMobile.classList.toggle("pl-menu-mob-out");
 
-  if (menuMobileOverlay.classList.contains('pl-vanish')){
-    menuMobileOverlay.classList.toggle('pl-toggleDisplayBlock');
-    setTimeout(function(){
-      menuMobileOverlay.classList.remove('pl-vanish');
-    }, 10);
-  }else{
-    menuMobileOverlay.classList.add('pl-vanish');
-    setTimeout(function(){
+    if (menuMobileOverlay.classList.contains('pl-vanish')){
       menuMobileOverlay.classList.toggle('pl-toggleDisplayBlock');
-    }, 500);
-  }
+      setTimeout(function(){
+        menuMobileOverlay.classList.remove('pl-vanish');
+      }, 10);
+    }else{
+      menuMobileOverlay.classList.add('pl-vanish');
+      setTimeout(function(){
+        menuMobileOverlay.classList.toggle('pl-toggleDisplayBlock');
+      }, 500);
+    }
 
-  if (btnMenuMobileIcon.getAttribute('name') === 'menu') {
-    setTimeout(function(){
-      btnMenuMobileIcon.setAttribute('name', 'close');
-    }, 200);
-  }else{
-    setTimeout(function(){
-      btnMenuMobileIcon.setAttribute('name', 'menu');
+    if (btnMenuMobileIcon.getAttribute('name') === 'menu') {
+      setTimeout(function(){
+        btnMenuMobileIcon.setAttribute('name', 'close');
       }, 200);
-  }
-});
+    }else{
+      setTimeout(function(){
+        btnMenuMobileIcon.setAttribute('name', 'menu');
+        }, 200);
+    }
+  });
+}
